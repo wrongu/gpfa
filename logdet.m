dad59612503335437c9cdf170c50a7db2d207a5b
wrongu <rlange@ur.rochester.edu>
2018-06-06 10:36:02 -0400

    removed non-class files

--- logdet.m
+++
@@ -1,6 +0,0 @@
-function d = logdet(X)
-
-L = chol(X);
-d = 2 * sum(log(diag(L)));
-
-end
--- predict.m
+++
@@ -1,6 +0,0 @@
-function predY = predict(Y, diagD, C, Kfull)
-
-[mu, ~] = inferX(Y, diagD, C, Kfull);
-predY = mu * C';
-
-end
